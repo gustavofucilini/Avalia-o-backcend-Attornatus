a40c44f776ec6c1581a8d7ba1a282e3c7f94ab40
Gustavo Gabriel Fucilini <gustavofucilini@gmail.com>
2023-02-07 21:00:53 -0300

    Primeiro commit

---
+++ src/main/java/br/com/attornatus/avaliacao/AvaliacaoJavaAttornatusApplication.java
@@ -0,0 +1,13 @@
+package br.com.attornatus.avaliacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvaliacaoJavaAttornatusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvaliacaoJavaAttornatusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/attornatus/avaliacao/AvaliacaoJavaAttornatusApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.attornatus.avaliacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvaliacaoJavaAttornatusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
